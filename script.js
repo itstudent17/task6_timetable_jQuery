311fbb5ea025a3f71c2252d98a291b90d273b49a
itstudent17 <itstudent17@outlook.com>
2019-05-20 00:36:30 +0400

    2

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 //сделать наследование для групп студентов
 
-var groupNumber, disciplineName;
+var groupNumber, disciplineName, tutorName;
 
 var group1 = {
     id: "Группа 1",
     students: ["Студент 1", "Студент 2", "Студент 3"],
     disciplines: ["Предмет 1", "Предмет 2", "Предмет 3"],
-    tutors: ["д.т.н. Техник. И.К.", "к.ф.н. Философ. А.А.", "д.ф.н. Конфуций Й.Ц."]
+    tutors: ["д.т.н. Техник И.К.", "к.ф.н. Философ А.А.", "д.ф.н. Конфуций Й.Ц."]
 }
 
 var group2 = {
     id: "Группа 2",
     students: ["Студент 4", "Студент 5", "Студент 6"],
     disciplines: ["Предмет 1", "Предмет 2", "Предмет 3"],
-    tutors: ["д.т.н. Техник. И.К.", "к.ф.н. Философ. А.А.", "д.ф.н. Конфуций Й.Ц."]
+    tutors: ["д.т.н. Техник И.К.", "к.ф.н. Философ А.А.", "д.ф.н. Конфуций Й.Ц."]
 }
 
 var group3 = {
     id: "Группа 3",
     students: ["Студент 7", "Студент 8", "Студент 9"],
     disciplines: ["Предмет 1", "Предмет 2", "Предмет 3"],
-    tutors: ["д.т.н. Техник. И.К.", "к.ф.н. Философ. А.А.", "д.ф.н. Конфуций Й.Ц."]
+    tutors: ["д.т.н. Техник И.К.", "к.ф.н. Философ А.А.", "д.ф.н. Конфуций Й.Ц."]
 }
 
 $('p.groups').click(function () {
@@ -39,11 +39,20 @@ $('p.groups').click(function () {
             $('p.discipline:eq(0)').text(group1.disciplines[0] + " (" + group1.tutors[0] + ")");
             $('p.discipline:eq(1)').text(group1.disciplines[1] + " (" + group1.tutors[1] + ")");
             $('p.discipline:eq(2)').text(group1.disciplines[2] + " (" + group1.tutors[2] + ")");
+            break;
 
+        case "Группа 2":
+            $('p.discipline:eq(0)').text(group2.disciplines[0] + " (" + group2.tutors[0] + ")");
+            $('p.discipline:eq(1)').text(group2.disciplines[1] + " (" + group2.tutors[1] + ")");
+            $('p.discipline:eq(2)').text(group2.disciplines[2] + " (" + group2.tutors[2] + ")");
+            break;
 
-
-        break;
-    
+        case "Группа 3":
+            $('p.discipline:eq(0)').text(group3.disciplines[0] + " (" + group3.tutors[0] + ")");
+            $('p.discipline:eq(1)').text(group3.disciplines[1] + " (" + group3.tutors[1] + ")");
+            $('p.discipline:eq(2)').text(group3.disciplines[2] + " (" + group3.tutors[2] + ")");
+            break;
+        
         default:
             break;
     }
@@ -51,17 +60,24 @@ $('p.groups').click(function () {
     
 
 });
-/*
+
+
 $('p.discipline').click(function () {
     $(this).css('font-weight','bold');
     disciplineName = $(this).text();
-    console.log(disciplineName);
-    $('#description_first_part').text(`Посещаемость занятий по предмету обучения «${disciplineName}» ` + `студентами группы «${groupNumber}» `+ `${disciplineName}` );
-    $('#description_second_part').text(`преподаватель ${disciplineName}` );
-    console.log(groupNumber + disciplineName);
+    $('#description').html(`Посещаемость занятий по предмету обучения <b>«${disciplineName.split(" (")[0]}»</b> ` 
+    + `студентами группы <b>«${groupNumber}»</b>, ` + ` преподаватель <b>${disciplineName.split(" (")[1]}</b>`);
+    //убрать лишнюю скобку
+    //перенести преподавателя на новую строку
+
+    //console.log(groupNumber + disciplineName);
+
+    for (let index = 0; index < 3; index++) {
+        $("#info").after(`<tr><td>${group1.students[index]}</td><td>1</td><td>1</td><tr>`)
+        
+    }
     
 });
-*/
  
 
 
@@ -72,6 +88,8 @@ if (groupNumber != "" && disciplineName != "") {
     console.log("Nothing");
     
 }*/
+
+
 
 
 
