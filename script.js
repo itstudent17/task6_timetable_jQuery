4ff85d748c7397c4f3c7d92ab3c9eb6d6a27350b
itstudent17 <49056212+itstudent17@users.noreply.github.com>
2019-05-20 16:07:27 +0300

    3

--- script.js
+++ script.js
@@ -2,35 +2,39 @@
 
 var groupNumber, disciplineName, tutorName;
 
+var group1 = {};
+
 var group1 = {
     id: "Группа 1",
-    students: ["Студент 1", "Студент 2", "Студент 3"],
+    students: ["Гр1Ст1", "Гр1Ст2", "Гр1Ст3"],
     disciplines: ["Предмет 1", "Предмет 2", "Предмет 3"],
     tutors: ["д.т.н. Техник И.К.", "к.ф.н. Философ А.А.", "д.ф.н. Конфуций Й.Ц."]
 }
 
 var group2 = {
     id: "Группа 2",
-    students: ["Студент 4", "Студент 5", "Студент 6"],
+    students: ["Гр2Ст1", "Гр2Ст2", "Гр2Ст3"],
     disciplines: ["Предмет 1", "Предмет 2", "Предмет 3"],
     tutors: ["д.т.н. Техник И.К.", "к.ф.н. Философ А.А.", "д.ф.н. Конфуций Й.Ц."]
 }
 
 var group3 = {
     id: "Группа 3",
-    students: ["Студент 7", "Студент 8", "Студент 9"],
+    students: ["Гр3Ст1", "Гр3Ст2", "Гр3Ст3"],
     disciplines: ["Предмет 1", "Предмет 2", "Предмет 3"],
     tutors: ["д.т.н. Техник И.К.", "к.ф.н. Философ А.А.", "д.ф.н. Конфуций Й.Ц."]
 }
 
-$('p.groups').click(function () {
-    $(this).css('font-weight','bold');//снять выделение у прочих абзацев
-    groupNumber = $(this).text();
+//при смене группы при уже выбранной паре группа - предмет в блоке с информацией изменений не происходит 
 
+$('p.groups').click(function () {
+    $('p.groups').css('font-weight','normal');
+    $(this).css('font-weight','bold');
+    groupNumber = $(this).text();
+    console.log(groupNumber);
+    
     switch (groupNumber) {
         case "Группа 1":
-            console.log(group1.disciplines.length);
-            console.log(group1.disciplines[0]);
             //здесь нужно сделать циклом
             /*for (let i = 0; i < group1.disciplines.length; i++) {
             $('p.discipline:eq(3 + i)').text(group1.disciplines[i]);
@@ -56,27 +60,37 @@ $('p.groups').click(function () {
         default:
             break;
     }
-
-    
-
 });
 
-
+//блок кода для выбора предмета и добавления соответствующей информкации в блок с посещаемостью
 $('p.discipline').click(function () {
+    //выделение жирным предмета, на который пришелся клик
+    $('p.discipline').css('font-weight','normal');
     $(this).css('font-weight','bold');
+
+    //добавление текста в блок с информацией о посещаемости
     disciplineName = $(this).text();
     $('#description').html(`Посещаемость занятий по предмету обучения <b>«${disciplineName.split(" (")[0]}»</b> ` 
     + `студентами группы <b>«${groupNumber}»</b>, ` + ` преподаватель <b>${disciplineName.split(" (")[1]}</b>`);
+
+
     //убрать лишнюю скобку
     //перенести преподавателя на новую строку
 
+    //список студентов только возрастает, нужно. чтобы убирался
+
     //console.log(groupNumber + disciplineName);
+    $('tr.newly_added').remove();
 
     for (let index = 0; index < 3; index++) {
-        $("#info").after(`<tr><td>${group1.students[index]}</td><td>1</td><td>1</td><tr>`)
+        $("#info").after(`<tr class="newly_added"><td class="light_grey">${group1.students[index]}</td><td></td><td></td><td></td><td></td></tr>`);
         
-    }
-    
+    }    
+});
+
+$('button').click(function() {
+    alert("После перезагрузки страницы выберете группу другую студентов");
+    location.reload();
 });
  
 
@@ -89,7 +103,15 @@ if (groupNumber != "" && disciplineName != "") {
     
 }*/
 
+//как быть с датами?
+//нужно поля 
+/*
+В блоке "Список групп" отображны три группы.
 
+
+
+
+*/
 
 
 
