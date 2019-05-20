90b432a996701ba7e6d938d6a95d59eb79f2a38d
itstudent17 <49056212+itstudent17@users.noreply.github.com>
2019-05-20 17:50:38 +0300

    1

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 //сделать наследование для групп студентов
+//на уровне объектов можно добавить сортировку фамилий студентов методом sort()
 
 var groupNumber, disciplineName, tutorName;
 
@@ -8,24 +9,29 @@ var group1 = {
     id: "Группа 1",
     students: ["Гр1Ст1", "Гр1Ст2", "Гр1Ст3"],
     disciplines: ["Предмет 1", "Предмет 2", "Предмет 3"],
-    tutors: ["д.т.н. Техник И.К.", "к.ф.н. Философ А.А.", "д.ф.н. Конфуций Й.Ц."]
+    tutors: ["д.т.н. Техник И.К.", "к.ф.н. Философ А.А.", "д.ф.н. Конфуций Й.Ц."],
+    presence: [["v", "v", "v", "v"], ["x", "v", "x", "v"], ["x", "x", "x", "x"]]
 }
 
 var group2 = {
     id: "Группа 2",
     students: ["Гр2Ст1", "Гр2Ст2", "Гр2Ст3"],
     disciplines: ["Предмет 1", "Предмет 2", "Предмет 3"],
-    tutors: ["д.т.н. Техник И.К.", "к.ф.н. Философ А.А.", "д.ф.н. Конфуций Й.Ц."]
+    tutors: ["д.т.н. Техник И.К.", "к.ф.н. Философ А.А.", "д.ф.н. Конфуций Й.Ц."],
+    presence: [["v", "v", "v", "v"], ["x", "v", "x", "v"], ["x", "x", "x", "x"]]
 }
 
 var group3 = {
     id: "Группа 3",
     students: ["Гр3Ст1", "Гр3Ст2", "Гр3Ст3"],
     disciplines: ["Предмет 1", "Предмет 2", "Предмет 3"],
-    tutors: ["д.т.н. Техник И.К.", "к.ф.н. Философ А.А.", "д.ф.н. Конфуций Й.Ц."]
+    tutors: ["д.т.н. Техник И.К.", "к.ф.н. Философ А.А.", "д.ф.н. Конфуций Й.Ц."],
+    presence: [["v", "v", "v", "v"], ["x", "v", "x", "v"], ["x", "x", "x", "x"]]
 }
 
-//при смене группы при уже выбранной паре группа - предмет в блоке с информацией изменений не происходит 
+//при смене группы при уже выбранной паре группа - предмет в блоке с информацией изменений не происходит
+
+alert("Выберите нужную группу из списка групп");
 
 $('p.groups').click(function () {
     $('p.groups').css('font-weight','normal');
@@ -33,85 +39,107 @@ $('p.groups').click(function () {
     groupNumber = $(this).text();
     console.log(groupNumber);
     
+    //далее в зависимости от номера группы создаем список дисциплин для каждой выбранной группы
     switch (groupNumber) {
         case "Группа 1":
-            //здесь нужно сделать циклом
-            /*for (let i = 0; i < group1.disciplines.length; i++) {
-            $('p.discipline:eq(3 + i)').text(group1.disciplines[i]);
-
-        };*/
-            $('p.discipline:eq(0)').text(group1.disciplines[0] + " (" + group1.tutors[0] + ")");
-            $('p.discipline:eq(1)').text(group1.disciplines[1] + " (" + group1.tutors[1] + ")");
-            $('p.discipline:eq(2)').text(group1.disciplines[2] + " (" + group1.tutors[2] + ")");
+            $('p.discipline').remove();
+            for (let index = group1.disciplines.length - 1; index >= 0; index--) {
+                $('#disciplines').after(`<p class='discipline'>${group1.disciplines[index]}` + " (" + `${group1.tutors[index]}` + ")</p>");     
+            }
             break;
 
         case "Группа 2":
-            $('p.discipline:eq(0)').text(group2.disciplines[0] + " (" + group2.tutors[0] + ")");
-            $('p.discipline:eq(1)').text(group2.disciplines[1] + " (" + group2.tutors[1] + ")");
-            $('p.discipline:eq(2)').text(group2.disciplines[2] + " (" + group2.tutors[2] + ")");
+            $('p.discipline').remove();
+            for (let index = group2.disciplines.length - 1; index >= 0; index--) {
+                $('#disciplines').after(`<p class='discipline'>${group2.disciplines[index]}` + " (" + `${group2.tutors[index]}` + ")</p>");     
+            }
             break;
 
         case "Группа 3":
-            $('p.discipline:eq(0)').text(group3.disciplines[0] + " (" + group3.tutors[0] + ")");
-            $('p.discipline:eq(1)').text(group3.disciplines[1] + " (" + group3.tutors[1] + ")");
-            $('p.discipline:eq(2)').text(group3.disciplines[2] + " (" + group3.tutors[2] + ")");
+            $('p.discipline').remove();
+            for (let index = group2.disciplines.length - 1; index >= 0; index--) {
+                $('#disciplines').after(`<p class='discipline'>${group3.disciplines[index]}` + " (" + `${group3.tutors[index]}` + ")</p>");     
+            }
             break;
         
         default:
             break;
     }
-});
 
-//блок кода для выбора предмета и добавления соответствующей информкации в блок с посещаемостью
-$('p.discipline').click(function () {
+    alert("Выберите предмет из списка предметов обучения");
+
+
+    //блок кода для выбора предмета и добавления соответствующей информации в блок с посещаемостью
+    $('p.discipline').click(function () {
+
     //выделение жирным предмета, на который пришелся клик
     $('p.discipline').css('font-weight','normal');
     $(this).css('font-weight','bold');
 
     //добавление текста в блок с информацией о посещаемости
     disciplineName = $(this).text();
-    $('#description').html(`Посещаемость занятий по предмету обучения <b>«${disciplineName.split(" (")[0]}»</b> ` 
+    if (groupNumber != false && disciplineName != false) {
+        $('#description').html(`Посещаемость занятий по предмету обучения <b>«${disciplineName.split(" (")[0]}»</b> ` 
     + `студентами группы <b>«${groupNumber}»</b>, ` + ` преподаватель <b>${disciplineName.split(" (")[1]}</b>`);
+    }
+    
+    
+
 
 
     //убрать лишнюю скобку
     //перенести преподавателя на новую строку
 
-    //список студентов только возрастает, нужно. чтобы убирался
-
-    //console.log(groupNumber + disciplineName);
     $('tr.newly_added').remove();
 
-    for (let index = 0; index < 3; index++) {
-        $("#info").after(`<tr class="newly_added"><td class="light_grey">${group1.students[index]}</td><td></td><td></td><td></td><td></td></tr>`);
+    switch (groupNumber) {
+        case "Группа 1":
+                for (let index = group1.students.length - 1; index >= 0; index--) {
+                    $("#info").after(`<tr class="newly_added"><td class="light_grey">${group1.students[index]}</td><td>${group1.presence[0,0]}</td><td></td><td></td><td></td></tr>`);
+                };
+                break;
+
+        case "Группа 2":
+                for (let index = group2.disciplines.length - 1; index >= 0; index--) {//индекс исправить
+                    $("#info").after(`<tr class="newly_added"><td class="light_grey">${group2.students[index]}</td><td>${group1.presence[0,0]}</td><td></td><td></td><td></td></tr>`);
+                }; 
+                break;
+
+        case "Группа 3":
+                for (let index = group3.disciplines.length - 1; index >= 0; index--) {//индекс исправить
+                    $("#info").after(`<tr class="newly_added"><td class="light_grey">${group3.students[index]}</td><td>${group1.presence[0,0]}</td><td></td><td></td><td></td></tr>`);
+                };   
+                break;
+
+        default:
+            break;
+    }
+
+
         
-    }    
+       
 });
+
+
+
+
+
+});
+
+
+
+
+
+
 
 $('button').click(function() {
-    alert("После перезагрузки страницы выберете группу другую студентов");
     location.reload();
 });
- 
 
-
-/*
-if (groupNumber != "" && disciplineName != "") {
-    
-} else {
-    console.log("Nothing");
-    
-}*/
 
 //как быть с датами?
-//нужно поля 
-/*
-В блоке "Список групп" отображны три группы.
+//поля для просмотра отметок 
 
-
-
-
-*/
 
 
 
