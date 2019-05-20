527a15ee44496104b5eafb381823e615316a5855
itstudent17 <49056212+itstudent17@users.noreply.github.com>
2019-05-20 23:27:20 +0300

    3

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-//сделать наследование для групп студентов
-//на уровне объектов можно добавить сортировку фамилий студентов методом sort()
+/*
+Данные по названию группы, фамилиям студентов и преподавателей, перечню дисциплин и посещаемости хранятся в JSON-объектах.
+Всего 3 группы.
+В каждой группе по 3 студента; названия предметов и ФИО преподавателей с учеными степенями для одинаковых предметов в разных группах одинаковы.
+(На уровне массивов с фамилиями студентов можно добавить сортировку фамилий методом sort().)
 
-var groupNumber, disciplineName, tutorName;
+*/
 
-var group1 = {};
+//группы в формате JSON-объектов
+var group1 = '{"id": "Группа 1", "students": ["Гр1Ст1", "Гр1Ст2", "Гр1Ст3"], "disciplines": ["Предмет 1", "Предмет 2", "Предмет 3"], "tutors": ["д.т.н. Техник И.К.", "к.ф.н. Философ А.А.", "д.ф.н. Конфуций Й.Ц."], "presence": [["v", "v", "v", "v"], ["x", "v", "x", "v"], ["x", "x", "x", "x"]]}';
+var group2 = '{"id": "Группа 2", "students": ["Гр2Ст1", "Гр2Ст2", "Гр2Ст3"], "disciplines": ["Предмет 1", "Предмет 2", "Предмет 3"], "tutors": ["д.т.н. Техник И.К.", "к.ф.н. Философ А.А.", "д.ф.н. Конфуций Й.Ц."], "presence": [["v", "x", "v", "x"], ["x", "v", "x", "x"], ["x", "x", "x", "x"]]}';
+var group3 = '{"id": "Группа 3", "students": ["Гр3Ст1", "Гр3Ст2", "Гр3Ст3"], "disciplines": ["Предмет 1", "Предмет 2", "Предмет 3"], "tutors": ["д.т.н. Техник И.К.", "к.ф.н. Философ А.А.", "д.ф.н. Конфуций Й.Ц."], "presence": [["v", "x", "v", "x"], ["x", "v", "x", "x"], ["x", "x", "x", "x"]]}';
 
+/*
 var group1 = {
     id: "Группа 1",
     students: ["Гр1Ст1", "Гр1Ст2", "Гр1Ст3"],
@@ -18,7 +25,7 @@ var group2 = {
     students: ["Гр2Ст1", "Гр2Ст2", "Гр2Ст3"],
     disciplines: ["Предмет 1", "Предмет 2", "Предмет 3"],
     tutors: ["д.т.н. Техник И.К.", "к.ф.н. Философ А.А.", "д.ф.н. Конфуций Й.Ц."],
-    presence: [["v", "v", "v", "v"], ["x", "v", "x", "v"], ["x", "x", "x", "x"]]
+    presence: [["v", "x", "v", "x"], ["x", "v", "x", "x"], ["x", "x", "x", "x"]]
 }
 
 var group3 = {
@@ -28,16 +35,23 @@ var group3 = {
     tutors: ["д.т.н. Техник И.К.", "к.ф.н. Философ А.А.", "д.ф.н. Конфуций Й.Ц."],
     presence: [["v", "v", "v", "v"], ["x", "v", "x", "v"], ["x", "x", "x", "x"]]
 }
+*/
 
-//при смене группы при уже выбранной паре группа - предмет в блоке с информацией изменений не происходит
+//преобразуемм их к виду JavaScript-объектов
+group1 = JSON.parse(group1);
+group2 = JSON.parse(group2);
+group3 = JSON.parse(group3);
 
+//переменные для получения необходимых свойств JavaScript-объектов
+var groupNumber, disciplineName, tutorName;
+
+//запуск приложения с выбором пользолвателем группы студентов
 alert("Выберите нужную группу из списка групп");
 
 $('p.groups').click(function () {
     $('p.groups').css('font-weight','normal');
-    $(this).css('font-weight','bold');
+    $(this).css('font-weight','bold');//выделение выбранной группы
     groupNumber = $(this).text();
-    console.log(groupNumber);
     
     //далее в зависимости от номера группы создаем список дисциплин для каждой выбранной группы
     switch (groupNumber) {
@@ -65,98 +79,60 @@ $('p.groups').click(function () {
         default:
             break;
     }
-
+    
+    //далее пользователь выбирает предмет из появившегося после выбора группы списка
     alert("Выберите предмет из списка предметов обучения");
-
 
     //блок кода для выбора предмета и добавления соответствующей информации в блок с посещаемостью
     $('p.discipline').click(function () {
 
-    //выделение жирным предмета, на который пришелся клик
     $('p.discipline').css('font-weight','normal');
-    $(this).css('font-weight','bold');
+    $(this).css('font-weight','bold');//выделение жирным предмета, на который пришелся клик
 
     //добавление текста в блок с информацией о посещаемости
     disciplineName = $(this).text();
     if (groupNumber != false && disciplineName != false) {
         $('#description').html(`Посещаемость занятий по предмету обучения <b>«${disciplineName.split(" (")[0]}»</b> ` 
-    + `студентами группы <b>«${groupNumber}»</b>, ` + ` преподаватель <b>${disciplineName.split(" (")[1]}</b>`);
+    + `студентами группы <b>«${groupNumber}»</b>, ` + ` <br>преподаватель <b>${disciplineName.split(" (")[1].split(")")[0]}</b>`);
     }
-    
-    
 
-
-
-    //убрать лишнюю скобку
-    //перенести преподавателя на новую строку
-
+    //далее циклом добавляем в блок с посещаемостью строки таблицы по каждому студенту группы для заданного предмета, прежде очищая строки после шапки таблицы от прошлых данных
     $('tr.newly_added').remove();
 
+    //напосредственно добавление строк с отметками посещаемости
     switch (groupNumber) {
         case "Группа 1":
                 for (let index = group1.students.length - 1; index >= 0; index--) {
-                    $("#info").after(`<tr class="newly_added"><td class="light_grey">${group1.students[index]}</td><td>${group1.presence[index][index]}</td><td>${group1.presence[index][index]}</td><td>${group1.presence[index][index]}</td><td>${group1.presence[index][index]}</td></tr>`);
+                    $("#info").after(`<tr class="newly_added"><td class="light_grey">${group1.students[index]}</td><td>${group1.presence[index][0]}</td><td>${group1.presence[index][1]}</td><td>${group1.presence[index][2]}</td><td>${group1.presence[index][3]}</td></tr>`);
                 };
                 break;
 
         case "Группа 2":
                 for (let index = group2.disciplines.length - 1; index >= 0; index--) {
-                    $("#info").after(`<tr class="newly_added"><td class="light_grey">${group2.students[index]}</td><td>${group2.presence[index][index]}</td><td>${group2.presence[index][index]}</td><td>${group2.presence[index][index]}</td><td>${group2.presence[index][index]}</td></tr>`);
+                    $("#info").after(`<tr class="newly_added"><td class="light_grey">${group2.students[index]}</td><td>${group2.presence[index][0]}</td><td>${group2.presence[index][1]}</td><td>${group2.presence[index][2]}</td><td>${group2.presence[index][3]}</td></tr>`);
                 }; 
                 break;
 
         case "Группа 3":
                 for (let index = group3.disciplines.length - 1; index >= 0; index--) {
-                    $("#info").after(`<tr class="newly_added"><td class="light_grey">${group3.students[index]}</td><td>${group3.presence[index][index]}</td><td>${group3.presence[index][index]}</td><td>${group3.presence[index][index]}</td><td>${group3.presence[index][index]}</td></tr>`);
+                    $("#info").after(`<tr class="newly_added"><td class="light_grey">${group3.students[index]}</td><td>${group3.presence[index][0]}</td><td>${group3.presence[index][1]}</td><td>${group3.presence[index][2]}</td><td>${group3.presence[index][3]}</td></tr>`);
                 };   
                 break;
 
         default:
             break;
-    }
-
-
-        
-       
+    }     
+});
 });
 
-
-
-
-
-});
-
-
-var arr = [[1,2], [3,4]];
-console.log(arr[0][0]);
-
-
-
-
-
+//кнопка для быстрой перезагруки страницы
 $('button').click(function() {
     location.reload();
 });
 
 
 //как быть с датами?
-//поля для просмотра отметок 
+//поля для просмотра или для внесения отметок 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//сделать наследование для групп студентов
+//
